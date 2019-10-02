a60496ba15642f2a76b37756f6e108b14223dd2c
vasghausen <wildvas@yandex.ru>
2019-10-02 10:21:50 +0300

    Мake it more beautiful.

--- Lesson001/Task4/Program.cs
+++ Lesson001/Task4/Program.cs
@@ -6,29 +6,31 @@ namespace Task4
     {
         public float Cooldown { get; private set; }
         public int Damage { get; private set; }
+
         public bool IsReloading()
         {
             throw new NotImplementedException();
         }
     }
 
-    class MovementParameters
+    class Movement
     {
-        public float MovementDirectionX { get; private set; }
-        public float MovementDirectionY { get; private set; }
-        public float MovementSpeed { get; private set; }
+        public float DirectionX { get; private set; }
+        public float DirectionY { get; private set; }
+        public float Speed { get; private set; }
+
+        public void Move()
+        {
+            //Do move
+        }
     }
 
     class Player
     {
         public string Name { get; private set; }
         public int Age { get; private set; }
-        public MovementParameters MovementParameters { get; private set; }
+        public Movement MovementParameters { get; private set; }
         public Weapon Weapon { get; private set; }
-        public void Move()
-        {
-            //Do move
-        }
 
         public void Attack()
         {
